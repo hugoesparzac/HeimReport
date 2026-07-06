e01b5e1a0e546ff701fea29e64a924d811298723
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2026-07-05 20:24:51 -0600

    feat(database): add mappers

--- src/HeimReport.Api/Mappers/DepartmentMapper.cs
+++ src/HeimReport.Api/Mappers/DepartmentMapper.cs
@@ -1,6 +1,20 @@
+using HeimReport.Api.DTOs.Departments;
+using HeimReport.Api.Entities;
+
 namespace HeimReport.Api.Mappers;
 
-public class DepartmentMapper
+public static class DepartmentMapper
 {
-    
+    public static DepartmentResponseDto ToResponseDto(this Department department)
+    {
+        return new DepartmentResponseDto(department.Id, department.Name);
+    }
+
+    public static Department ToEntity(this DepartmentCreateUpdateDto dto)
+    {
+        return new Department
+        {
+            Name = dto.Name,
+        };
+    }
 }
--- src/HeimReport.Api/Mappers/PositionMapper.cs
+++ src/HeimReport.Api/Mappers/PositionMapper.cs
@@ -1,6 +1,27 @@
+using HeimReport.Api.DTOs.Positions;
+using HeimReport.Api.Entities;
+
 namespace HeimReport.Api.Mappers;
 
-public class PositionMapper
+public static class PositionMapper
 {
+    public static PositionResponseDto ToResponseDto(this Position position)
+    {
+        return new PositionResponseDto(
+            position.Id, 
+            position.Title, 
+            position.CareerLevel, 
+            position.IsCritical
+        );
+    }
     
+    public static Position ToEntity(this PositionCreateUpdateDto dto)
+    {
+        return new Position
+        {
+            Title = dto.Title,
+            CareerLevel = dto.CareerLevel,
+            IsCritical = dto.IsCritical
+        };
+    }
 }
