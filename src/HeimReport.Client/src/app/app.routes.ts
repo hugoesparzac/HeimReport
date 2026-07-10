8a92d4856bb5d7fc597c1d3ca9cb2da44029fe2e
jeferson ramos <chessjr10p9@gmail.com>
2026-07-10 10:44:58 -0500

    Creating the feature-based architecture structure for login

--- src/HeimReport.Client/src/app/app.routes.ts
+++ src/HeimReport.Client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/feature/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
