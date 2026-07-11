68cc4525c3f6ce4edb7c81eb7a44faabebc903c5
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2026-07-11 14:29:50 -0600

    refactor(JWT): refactor JWT and GlobalExceptionHandler

--- src/HeimReport.Api/Program.cs
+++ src/HeimReport.Api/Program.cs
@@ -1,14 +1,10 @@
-using System.Text;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using HeimReport.Api.Data;
 using HeimReport.Api.ExceptionHandlers;
 using HeimReport.Api.Extensions;
-using HeimReport.Api.Security;
 using HeimReport.Api.Validators.Auth;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,33 +18,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddRepositories();
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 builder.Services.AddValidatorsFromAssemblyContaining<UserRegistrationDtoValidator>();
 builder.Services.AddFluentValidationAutoValidation();
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
-
-builder.Services.AddScoped<IJwtProvider, JwtProvider>();
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-        };
-    });
-
-builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -61,10 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
