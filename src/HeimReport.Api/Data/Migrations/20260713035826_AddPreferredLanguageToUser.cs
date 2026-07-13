e8beb2f6098caa93cf33694b4b555060e23ee7f9
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2026-07-12 22:00:28 -0600

    feat(database): add language migration

---
+++ src/HeimReport.Api/Data/Migrations/20260713035826_AddPreferredLanguageToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeimReport.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreferredLanguageToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PreferredLanguage",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferredLanguage",
+                table: "Users");
+        }
+    }
+}
