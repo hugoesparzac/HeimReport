19e3e693dc9f4df52e80716c29efd5d2c7b3467a
wanderboy131 <munozwanderley131@gmail.com>
2026-07-12 20:32:41 -0500

    feat: Create Dashboard prediction view and rearrange of the structure

---
+++ src/HeimReport.Client/src/app/features/dashboard/dashboard-page/components/trend-chart/trend-chart.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trend-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './trend-chart.html',
+  styleUrl: './trend-chart.css',
+})
+export class TrendChartComponent {}
