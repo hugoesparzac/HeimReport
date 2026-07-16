802f384e3f4bbe3f01778640b8d5d97f4dbcb2c5
jeferson ramos <chessjr10p9@gmail.com>
2026-07-16 11:29:05 -0500

    feature: implemented mapper for QuestionOptions

---
+++ src/HeimReport.Api/Mappers/QuestionOptionMapper.cs
@@ -0,0 +1,27 @@
+﻿using HeimReport.Api.DTOs.QuestionOption;
+using HeimReport.Api.Entities;
+
+namespace HeimReport.Api.Mappers;
+
+public static class QuestionOptionMapper
+{
+    public static QuestionOption ToEntity(this QuestionOptionCreateDto dto)
+    {
+        return new QuestionOption
+        {
+            QuestionId = dto.QuestionId,
+            Text = dto.Text,
+            Value = dto.Value,
+            OrderIndex = dto.OrderIndex
+        };
+    }
+
+
+    public static void ApplyUpdate(this QuestionOption questionOption, QuestionOptionUpdateDto dto)
+    {
+        questionOption.Text = dto.Text;
+        questionOption.OrderIndex = dto.OrderIndex;
+        questionOption.Value = dto.Value;
+    }
+}
+
