b3b00b1bdcfc6b00af3542f54e6b79fd26d7e9a6
jeferson ramos <chessjr10p9@gmail.com>
2026-07-15 23:33:43 -0500

    feature: implemented validators for Questions

---
+++ src/HeimReport.Api/Validators/Questions/QuestionUpdateDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using HeimReport.Api.DTOs.Questions;
+
+namespace HeimReport.Api.Validators.Questions;
+public class QuestionUpdateDtoValidator: AbstractValidator<QuestionUpdateDto>
+{
+    public QuestionUpdateDtoValidator()
+    {
+        RuleFor(x => x.Text)
+            .NotEmpty().WithMessage("Text is required")
+            .MaximumLength(500).WithMessage("Text must not exceed 500 characters");
+        RuleFor(x => x.OrderIndex)
+            .NotEmpty().WithMessage("OrderIndex is required")
+            .GreaterThanOrEqualTo(0).WithMessage("OrderIndex must be a non-negative integer");
+    }
+}
+
