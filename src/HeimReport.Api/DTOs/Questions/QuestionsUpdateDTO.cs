d8f525c81f779e246abe1ee4a0cd42d9c1f03e03
jeferson ramos <chessjr10p9@gmail.com>
2026-07-15 22:40:55 -0500

    fix: 'set' was changed to 'init' in the DTOs

--- src/HeimReport.Api/DTOs/Questions/QuestionsUpdateDTO.cs
+++ src/HeimReport.Api/DTOs/Questions/QuestionsUpdateDTO.cs
@@ -5,7 +5,7 @@ namespace HeimReport.Api.DTOs.Questions;
 
 public record QuestionsUpdateDTO
 {
-    public required int Id { get; set; }
+    public required int Id { get; init; }
     public required string Text { get; init; }
     public required QuestionType QuestionType { get; init; }
     public required int OrderIndex { get; init; }
