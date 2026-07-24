724c17c9adfa3f0cb17129ec7eda483462eec5c7
Hugo Esparza Castañeda <hugo.esparza3@gmail.com>
2026-07-23 22:35:16 -0600

    Register all three services in DI

--- src/HeimReport.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/HeimReport.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using HeimReport.Api.Services.Countries;
+using HeimReport.Api.Services.Departments;
+using HeimReport.Api.Services.Positions;
 using HeimReport.Api.Services.Users;
 
 namespace HeimReport.Api.Extensions;
@@ -6,6 +9,9 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddScoped<ICountryService, CountryService>();
+        services.AddScoped<IDepartmentService, DepartmentService>();
+        services.AddScoped<IPositionService, PositionService>();
         services.AddScoped<IUserService, UserService>();
 
         return services;
